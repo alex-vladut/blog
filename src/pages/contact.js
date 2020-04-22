9ab502d2df457a0e85ec3bd9abaca3f8fe90f74a
Alex Vladut <alexandruvladut@gmail.com>
2020-04-22 15:14:31 +0300

    feat(app): add contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,15 +2,33 @@ import React from "react";
 
 import { Layout } from "../components/layout";
 
+import "./contact.css";
+
 const Contact = () => (
   <Layout>
-    <h1>Contact</h1>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea dignissimos
-      aut consequuntur aspernatur corrupti ratione, odit similique tenetur
-      accusantium, est nostrum esse minus iure voluptatum nihil cumque
-      blanditiis non? Odit.
-    </p>
+    <div className="contact-background" />
+    <div className="contact-content">
+      <div className="contact-address">
+        <h2 className="contact-title">
+          <span className="has-secondary-color">Cluj-Napoca,</span> Romania
+        </h2>
+        <p className="contact-address">
+          166 Main Street, Beverly Hills, CA 90210
+        </p>
+      </div>
+      <div className="contact-form">
+        <input type="text" name="name" placeholder="Name" />
+        <input type="email" name="email" placeholder="Email" />
+        <input type="text" name="subject" placeholder="Subject (Optional)" />
+        <textarea
+          placeholder="Message"
+          name="your-message"
+          cols="40"
+          rows="6"
+        />
+        <button>Contact us</button>
+      </div>
+    </div>
   </Layout>
 );
 
