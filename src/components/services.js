f77fbeb3d42fe99d1a56b3ae421ef3ea5e208ddf
Alex Vladut <alexandruvladut@gmail.com>
2020-04-25 22:24:51 +0300

    fix(app): improve spacing on home page

--- src/components/services.js
+++ src/components/services.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import {
-  faLaptopCode,
   faMobileAlt,
-  faCode
+  faRocket,
+  faUserLock,
+  faLaptopCode
 } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
@@ -21,36 +22,44 @@ export const Services = () => (
         </div>
         <h4 className="title">Development</h4>
         <div className="content">
-          Working with client and community, we deliver masterplans that create
-          vibrant new places and spaces, attract people, and encourage
-          investment through.
+          We deliver applications that look great and delight your customers.
         </div>
       </div>
       <div>
         <div className="icon has-secondary-color">
           <FontAwesomeIcon icon={faMobileAlt} />
         </div>
-        <h4 className="title">Responsive Design</h4>
+        <h4 className="title">Responsive</h4>
         <div className="content">
-          Working with client and community, we deliver masterplans that create
-          vibrant new places and spaces, attract people, and encourage
-          investment through.
+          Your website will look great regardless from which devices your
+          customers will access it.
         </div>
       </div>
       <div>
         <div className="icon has-secondary-color">
-          <FontAwesomeIcon icon={faCode} />
+          <FontAwesomeIcon icon={faRocket} />
         </div>
-        <h4 className="title">Clean Code</h4>
+        <h4 className="title">Performant</h4>
         <div className="content">
-          Working with client and community, we deliver masterplans that create
-          vibrant new places and spaces, attract people, and encourage
-          investment through.
+          It's proven that users will leave your website if it doesn't load
+          quickly, so we will design the website from the start to be fast and
+          load instantaneously.
+        </div>
+      </div>
+      <div>
+        <div className="icon has-secondary-color">
+          <FontAwesomeIcon icon={faUserLock} />
+        </div>
+        <h4 className="title">Secure</h4>
+        <div className="content">
+          It's more important than ever to protect your users from malicious
+          attacks, so we will implement a high level of security right from the
+          start.
         </div>
       </div>
     </section>
     <p>
-      <a className="has-line-before" href="#">
+      <a className="has-line-before" href="#download">
         Download Resume
       </a>
     </p>
